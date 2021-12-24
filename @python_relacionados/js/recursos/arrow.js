5728973fd9ae295254831a183cb2b5d14d58a6da
Lucas-far <mizoguchia@hotmail.com>
2021-12-24 00:53:37 -0300

    novos dados

---
+++ @python_relacionados/js/recursos/arrow.js
@@ -0,0 +1,26 @@
+/*
+!@# recursos
+
+------- AULA -------
+O curso foi modificado. Aula não identificada
+
+------- DETALHE -------
+Procedimentos para criação de uma função curta (flecha) (arrow function) (=>)
+
+------- LEGENDAS -------
+1. Declaração
+2. Nome da função
+3. Nome de parâmetro
+4. Retorno (como 'return' em Python)
+5. ação da função
+*/
+
+//  1 2            3     4  5
+const cacha_alta = texto => texto.toUpperCase()
+
+// Chamada literal da função
+console.log('[1] ' + cacha_alta((texto = 'javascript')))
+
+// Chamada da função numa variável
+let objeto_ = cacha_alta((texto = 'js é a extensão de javascript'))
+console.log('[2] ' + objeto_)
--- @python_relacionados/js/recursos/charcodeat.js
+++ @python_relacionados/js/recursos/charcodeat.js
@@ -1,8 +1,16 @@
+/*
+!@# recursos
 
+------- AULA -------
+O curso foi modificado. Aula não identificada
 
-// Módulo: charcodeat.js
-// Aparição: aula_21._Tipos_em_JavaScript:_String.js
-// Objetivo: encontrar a posição de um caractere numa classe [ string ] e retornar seu inteiro
+------- DETALHE -------
+Encontrar a posição de um caractere numa classe [ string ] e retornar seu código inteiro
+
+------- LEGENDAS -------
+1. Se o índice é excedente, o retorno = NaN
+2. Uso em dados literais é permitido
+*/
 
 const palavra = '_@^L2' // essa constante têm 4 índices (contagem de índice inicia do zero)
 
@@ -11,9 +19,5 @@ console.log(`Caractere: @ possui código = ${palavra.charCodeAt(1)}`)
 console.log(`Caractere: ^ possui código = ${palavra.charCodeAt(2)}`)
 console.log(`Caractere: L possui código = ${palavra.charCodeAt(3)}`)
 console.log(`Caractere: 2 possui código = ${palavra.charCodeAt(4)}`)
-
-// se o índice é excedente, o retorno = NaN
-console.log(`Caractere:   possui código = ${palavra.charCodeAt(5)}`)
-
-// uso em dados literais é permitido
-console.log('Pamonha'.charCodeAt(2))
+console.log(`Caractere:   possui código = ${palavra.charCodeAt(5)}`) // 1
+console.log('Pamonha'.charCodeAt(2)) // 2
