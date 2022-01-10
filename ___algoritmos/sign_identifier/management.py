eb2f850f82f6305a8f1aaae2c98b925e7fb28297
Lucas-far <mizoguchia@hotmail.com>
2022-01-10 16:08:53 -0300

    new data

--- ___algoritmos/sign_identifier/management.py
+++ ___algoritmos/sign_identifier/management.py
@@ -2,36 +2,57 @@
 
 from database import (command)
 
+
+def show_database(database_):
+    for data in database_:
+        print(data)
+
+
+def store_database(database_main, database_box):
+    for data in database_main:
+        database_box.append(data)
+
+
+"The function below is specific to this database"
+
+
+# Numbers at var [ indexes ] relate to the number of fields this database has
+def json(target_table, json_table):
+
+    index_counter = 0
+    indexes = (0, 1, 2)  # from tuple database, nested indexes 0, 1, 2 will be called
+
+    while index_counter < len(target_table):
+
+        json_table.append(
+            {
+                'id': target_table[index_counter][indexes[0]],
+                'nome': target_table[index_counter][indexes[1]],
+                'nascimento': target_table[index_counter][indexes[2]],
+            }
+        )
+
+        index_counter += 1
+
+
 table_pessoas = []
 table_pessoas_json = []
 command.execute('SELECT * FROM pessoas;')
 
 if __name__ == '__main__':
-    for data in command:
-        print(data)
-        table_pessoas.append(data)
+
+    label_no_json = '------- BANCO DE DADOS NÃO JSON -------'
+    label_json = '------- BANCO DE DADOS JSON -------'
+
+    "Chamar bdd manualmente"
+    # for data in command:
+    #     print(data)
 
     print(table_pessoas)
-
-    list_length = 0
-    index0 = 0
-    index1 = 1
-    index2 = 2
-
-    while list_length < len(table_pessoas):
-        # table_pessoas[0][0]
-        # table_pessoas[0][1]
-        # table_pessoas[0][2]
-        table_pessoas_json.append(
-            {
-                'id': table_pessoas[list_length][index0],
-                'nome': table_pessoas[list_length][index1],
-                'nascimento': table_pessoas[list_length][index2],
-            }
-        )
-
-        list_length += 1
-
     print(table_pessoas_json)
-    for json in table_pessoas_json:
-        print(json)
+    store_database(database_main=command, database_box=table_pessoas)
+    json(target_table=table_pessoas, json_table=table_pessoas_json)
+    print(label_no_json)
+    show_database(database_=table_pessoas)
+    print(label_json)
+    show_database(database_=table_pessoas_json)
